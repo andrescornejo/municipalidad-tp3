2d1a26488f23eb1d64381e8932b072b94438c2dd
Andres <andrewcornejoh@gmail.com>
2020-07-27 21:10:26 -0600

    Finalized design for proof of payment page.

--- Pages/Client/consultarComprobantes.aspx.designer.cs
+++ Pages/Client/consultarComprobantes.aspx.designer.cs
@@ -112,14 +112,5 @@ namespace Muni.Pages.Client
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblModalTotal;
-
-        /// <summary>
-        /// btnCancel control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnCancel;
     }
 }

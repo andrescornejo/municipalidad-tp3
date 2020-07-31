48db430e17bc8c06515a275015a9ac151988721b
palpizarm <pabloalpizar99@gmail.com>
2020-07-30 22:48:34 -0600

    add generarRecibosAP procedure

--- sql/stored_procedures/sp_generarRecibosAP.sql
+++ sql/stored_procedures/sp_generarRecibosAP.sql
@@ -14,10 +14,45 @@ AS
 BEGIN
 	BEGIN TRY
 		SET NOCOUNT ON
-		DECLARE @tmpAP TABLE (id INT)
+		DECLARE @id INT
+		DECLARE @tmpAP udt_idTable
 
-        INSERT INTO @tmpAP (id)
-        
+        INSERT INTO @tmpAP (storedID)
+        SELECT AP.id
+        FROM [dbo].[AP] AP 
+		WHERE (SELECT CONVERT(DATE, AP.insertedAt)) = @inFecha
+
+
+		IF(EXISTS(SELECT 1 FROM @tmpAP))
+		BEGIN
+  			SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+			BEGIN TRANSACTION
+				-- insertar los recibos 
+				INSERT INTO [Recibo] (
+					idPropiedad,
+					idConceptoCobro,
+					idTipoEstado,
+					fecha,
+					fechaVencimiento,
+					monto,
+					activo
+				)
+				SELECT 
+					AP.idPropiedad,
+					C.id,
+					T.id,
+					@inFecha,
+					DATEADD(DAY,C.QDiasVencimiento,@inFecha),
+					AP.cuota,
+					1
+				FROM @tmpAP tmp
+				INNER JOIN [dbo].[ConceptoCobro] C ON C.nombre = 'AP'
+				INNER JOIN [dbo].[TipoEstadoRecibo] T ON T.estado = 'Pendiente'
+				INNER JOIN [dbo].[AP] AP ON tmp.storedID = AP.id 
+
+			COMMIT
+		END;
+
 
 	END TRY
 
